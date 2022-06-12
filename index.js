68de31d79dc851340160432bdc1756edaaba8acc
jvhill728 <jvhill728@gmail.com>
2022-06-12 15:47:52 -0500

    server set up

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 require('dotenv').config();
 const express = require('express');
+const server = express();
+
+const PORT = process.env.PORT || 4000;
+
+const cors = require('cors');
+server.use(cors());
+
+const morgan = require('morgan');
+server.use(express.json());
+
+const path = require('path');
+server.use(express.static(path.join(__dirname, 'build')));
+
+server.use('/api', require('./api'))
+
+const client = require('./db');
+
+const handle = server.listen(PORT, async () => {
+    console.log(`Server is running on ${PORT}!`);
+
+    try {
+        await client.connect();
+        console.log('Database is up and running!');
+    } catch (error) {
+        throw error
+    }
+});
+
+module.exporets = { server, handle };
