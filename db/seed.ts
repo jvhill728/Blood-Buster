25f2a4a29bd2efd0232cd72287a455625d034fd8
jvhill728 <jvhill728@gmail.com>
2022-06-12 19:34:25 -0500

    working on seed data, dropTables, createTables

--- db/seed.ts
+++ db/seed.ts
@@ -1,1 +1,32 @@
 const client = require('./client');
+
+async function dropTables() {
+    console.log('Dropping All Tables...');
+
+    try {
+        await client.query(`
+        DROP TABLE IF EXISTS movies;
+        DROP TABLE IF EXISTS users;
+        `);
+
+        console.log('Finshed dropping those tables!');
+    } catch (error) {
+        console.log('Error dropping tables!');
+        throw error;
+    }
+}
+
+async function createTables() {
+    console.log('Building those tables...');
+
+    try {
+        await client.query(`
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(255) UNIQUE NOT NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL
+            ); 
+            `);
+    }
+}
