b8e1afea5278a63a30a2aeefed5ff0c2d2239733
jvhill728 <jvhill728@gmail.com>
2022-06-12 20:15:18 -0500

    working on seed data. dropTables, createTables

--- db/seed.ts
+++ db/seed.ts
@@ -27,6 +27,11 @@ async function createTables() {
                 email VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL
             ); 
+            CREATE TABLE movies (
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(255) UNIQUE NOT NULL,
+                
+            )
             `);
     }
 }
